07bce66ae349c864df9ff6ce46ced362f5731389
yuliyaBarashka <zohn12333@gmail.com>
2026-05-07 21:28:32 +0700

    api

---
+++ src/storage/base_storage.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+class BaseStorage(ABC):
+
+    @abstractmethod
+    def add(self, item):
+        pass
+
+    @abstractmethod
+    def get_all(self):
+        pass
+
+    @abstractmethod
+    def delete(self, item):
+        pass
+
